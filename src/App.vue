5a11a4a6e2d50adb0eb05d1f8c53e3bfe8c3309d
yxiaojian33 <yang_xiaojian@qq.com>
2021-05-03 16:34:25 +0800

    导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button type="primary">el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view class="main"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
+<style lang="scss" rel="stylesheet/scss">
+@import "assets/style/index.scss";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+
+.main {
+  background: #ededed;
+  overflow: hidden;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'index',
+    redirect : 'home',
+    component: ()=> import('@/views/index'),
+    children :[
+      {
+        path: 'home',
+        component : ()=>import('@/views/home')
+      },
+      {
+        path: 'products',
+        component : ()=>import('@/views/products')
+      },
+      {
+        path: 'saleRecommend',
+        component : ()=>import('@/views/saleRecommend')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: ()=> import('@/views/login')
   }
 ]
 
